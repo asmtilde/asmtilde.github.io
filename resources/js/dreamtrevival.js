71e4a8877b54767584b1a993d66a68399e0830ee
jack <jack@asmtil.de>
2025-09-01 23:03:35 -0700

    opt?

---
+++ resources/js/dreamtrevival.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+const tileSize = 32;
+
+function update()
+{
+    // wake up 
+}
+
+function init()
+{
+    canvas.width = tileSize * 30;
+    canvas.height = tileSize * 25;
+}
